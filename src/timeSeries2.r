7323a5fe76b9fdc2ba390c3f1cd1b2a72964ee4e
Adam Sales <asales@utexas.edu>
2017-01-18 14:25:53 -0600

    fix time series

--- src/timeSeries2.r
+++ src/timeSeries2.r
@@ -1,11 +1,11 @@
 library(urca)
 data(npext)
-y <- ts(na.omit(npext$unemploy))
+y <- npext$unemploy
 y2 <- data.frame(Year=1890:1988,unemp=y)
 new <- read.csv('data/unemployment2.csv',header=F)
 new <- with(new,data.frame(year=V1,unemp=log(V16)))
 y <- c(y,new$unemp[new$year>1988])
-y <- ts(y)
+y <- ts(y,start=1890)
 
 lrTest <- function(p,edge=TRUE){
     stopifnot(p<20)
